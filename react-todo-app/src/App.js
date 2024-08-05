2d4e3e14739989e62edb771f3c1dcd15fbc63911
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-08-05 19:59:47 +0900

    Style : 버튼, 리스트 스타일링

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,6 +1,25 @@
 import React, {Component} from "react";
 import "./App.css";
 export default class App extends Component{
+
+  btnStyle={
+    color:"#fff",
+    border:"none",
+    padding:"5px 9px",
+    borderRadius:"50%",
+    cursor:"pointer",
+    float:"right"
+  }
+
+  getStyle=()=>{
+    return{
+      padding:"10px",
+      borderBottom:"1px #ccc dotted",
+      textDecoration:"none"
+    };
+  }
+
+
   render(){
     return(
       <div className="container">
@@ -8,6 +27,19 @@ export default class App extends Component{
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} />
+            공부하기
+            <button style={this.btnStyle}>x</button>
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} />
+            청소하기
+            <button style={this.btnStyle}>x</button>
+          </div>
+
         </div>
       </div>
     )
